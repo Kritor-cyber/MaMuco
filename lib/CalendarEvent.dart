61c84560c30315c8304f795651dabcf26565e5ca
Kritor-cyber <thibault.denier@efrei.net>
2021-03-31 08:47:50 +0200

    adding usefull function to CalendarEvent class

--- lib/CalendarEvent.dart
+++ lib/CalendarEvent.dart
@@ -4,9 +4,11 @@ import 'utilities.dart';
 class CalendarEvent {
   static Internationalization language;
 
-  DateTime startTime, endTime;
+  DateTime startTime = null, endTime = null;
   String title;
   String infos;
+  DateTime occurrence;  //Describe the time elapsed between two repetition of the event (if it's value equals 0 seconds, there is no repetition)
+  int numberRepetition; //Contain the number of times the event is repeat (useless value if no repetition, infinite repetition if it's value equals zero)
 
   static void setLanguage(Internationalization lang) { language = lang; }
 
@@ -14,17 +16,57 @@ class CalendarEvent {
   DateTime getEndTime() { return endTime; }
   String getTitle() { return title; }
   String getInfos() { return infos; }
+  DateTime getOccurrence() { return occurrence; }
+  int getRepetition() { return numberRepetition; }
 
-  void setStartTime(DateTime time) { startTime = time; }
-  void setEndTime(DateTime time) { endTime = time; }
+  /**
+   * return FALSE if the new start time isn't before the actual end time
+   */
+  bool setStartTime(DateTime time) {
+    if (endTime == null)
+      startTime = time;
+    else if (time.isBefore(endTime))
+      startTime = time;
+    else
+      return false;
+
+    return true;
+  }
+
+  /**
+   * return FALSE if the new end time isn't after the actual start time
+   */
+  bool setEndTime(DateTime time) {
+    if (startTime == null)
+      endTime = time;
+    else if (time.isAfter(startTime))
+      endTime = time;
+    else
+      return false;
+
+    return true;
+  }
+
+  /**
+   * return FALSE if the new end time isn't after the new start time
+   */
+  bool setTimes(DateTime start, DateTime end) {
+    if (start.isBefore(end)) {
+      startTime = start;
+      endTime = end;
+    }
+    else
+      return false;
+
+    return true;
+  }
+
   void setTitle(String newTitle) { title = newTitle; }
   void setInfos(String newInfos) { infos = newInfos; }
+  void setOccurrence(DateTime time) { occurrence = time; }
+  void setRepetition(int repetition) { numberRepetition = repetition; }
 
-  String getStartTimeString() {
-    return dateToString(startTime);
-  }
+  String getStartTimeString() { return dateToString(startTime); }
 
-  String getEndTimeString() {
-    return dateToString(endTime);
-  }
+  String getEndTimeString() { return dateToString(endTime); }
 }
