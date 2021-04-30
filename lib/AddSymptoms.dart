6340000c7cbd24acfcd8829fbe3ece1860158fad
elodie-bsst <elodie.bouisset@efrei.net>
2021-04-30 12:05:39 +0200

    Essai (avec paola) page enregistrement symptomes

--- lib/AddSymptoms.dart
+++ lib/AddSymptoms.dart
@@ -10,6 +10,30 @@ class AddSymptoms extends StatelessWidget {
       appBar: AppBar(
         title: Text(language.getAddSymptoms()),
       ),
+        body: Container(
+        //margin: EdgeInsets.only(top: 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                getSymptomsButton(context, Text("Respiration"), MaterialPageRoute(builder: (context) => AddSymptoms())),
+                getSymptomsButton(context, Text("Digestion"), MaterialPageRoute(builder: (context) => AddSymptoms())),
+                getSymptomsButton(context, Text("Humeur"), MaterialPageRoute(builder: (context) => AddSymptoms())),
+                getSymptomsButton(context, Text("Autres symptomes"), MaterialPageRoute(builder: (context) => AddSymptoms()))
+
+                ]
+            ),
+          ),
+    );
+  }
+  ElevatedButton getSymptomsButton (BuildContext context, Text title, MaterialPageRoute route) {
+    return ElevatedButton(
+      onPressed:(){
+      Navigator.push(context, route);
+    },
+    style : ButtonStyle (
+    backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+    ),
+    child: title,
     );
   }
 }
